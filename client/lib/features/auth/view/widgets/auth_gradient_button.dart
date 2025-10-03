898147f075a809852f7e28c4db721da6d2cfae51
Dean <deanfoster720@gmail.com>
2025-10-03 17:28:37 +0100

    Initial commit: Flutter project

---
+++ client/lib/features/auth/view/widgets/auth_gradient_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AuthGradientButton extends StatelessWidget {
+  const AuthGradientButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        fixedSize: const Size(395, 55),
+      ),
+      child: Text(
+        'Sign Up',
+        style: TextStyle(
+          fontSize: 17,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
+
+
